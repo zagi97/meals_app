937144e35d2f58256c0bbe82a22a0eae6fce2a71
Kruno Žagar <kruno.zagar@hivetech.hr>
2023-04-24 16:47:44 +0200

    first commit

---
+++ lib/screens/categories.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+
+import 'package:meals_app/data/dummy_data.dart';
+import 'package:meals_app/screens/meals.dart';
+import 'package:meals_app/widgets/category_grid_item.dart';
+import 'package:meals_app/models/category.dart';
+
+class CategoriesScreen extends StatelessWidget {
+  const CategoriesScreen({super.key});
+
+  void _selectCategory(BuildContext context, Category category) {
+    //ako taj meal sadrži taj id categorije koja je odabrana
+    final filteredMeals = dummyMeals
+        .where((meal) => meal.categories.contains(category.id))
+        .toList();
+    //Navigator.push(context, route)
+    Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (ctx) => MealsScreen(
+          title: category.title,
+          meals: filteredMeals,
+        ),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    //vrlo je vjerojatno kada imaš više screen-ova da će svaki screen
+    //koristiti Scaffold Widget da kreira osnovno uređenje zaslona
+    //naprimjer zbog toga što appBar se razlikuje od zaslona do zaslona...
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Pick your category"),
+      ),
+      body: GridView(
+        padding: const EdgeInsets.all(24),
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2,
+            childAspectRatio: 3 / 2,
+            crossAxisSpacing: 20,
+            mainAxisSpacing: 20),
+        children: [
+          //availableCategories.map((category) => CategoryGridItem(category: category)).toList();
+          for (final category in availableCategories)
+            CategoryGridItem(
+              category: category,
+              onSelectCategory: () {
+                //category je iz for loop-a
+                _selectCategory(context, category);
+              },
+            )
+        ],
+      ),
+    );
+  }
+}
