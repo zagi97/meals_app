385d2ee453c445688a0bdd7a3de1d905a2564d18
Kruno Žagar <kruno.zagar@hivetech.hr>
2023-04-26 10:53:17 +0200

    aplikacija je napravljena što se tiče 8 sekcije

--- lib/screens/categories.dart
+++ lib/screens/categories.dart
@@ -2,18 +2,23 @@ import 'package:flutter/material.dart';
 
 import 'package:meals_app/data/dummy_data.dart';
 import 'package:meals_app/models/meal.dart';
+import 'package:meals_app/models/category.dart';
 import 'package:meals_app/screens/meals.dart';
 import 'package:meals_app/widgets/category_grid_item.dart';
-import 'package:meals_app/models/category.dart';
 
 class CategoriesScreen extends StatelessWidget {
-  const CategoriesScreen({super.key, required this.onToggleFavorite});
+  const CategoriesScreen({
+    super.key,
+    required this.onToggleFavorite,
+    required this.availableMeals,
+  });
 
   final void Function(Meal meal) onToggleFavorite;
+  final List<Meal> availableMeals;
 
   void _selectCategory(BuildContext context, Category category) {
     //ako taj meal sadrži taj id categorije koja je odabrana
-    final filteredMeals = dummyMeals
+    final filteredMeals = availableMeals
         .where((meal) => meal.categories.contains(category.id))
         .toList();
     //Navigator.push(context, route)
